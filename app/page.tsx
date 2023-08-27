4779e35da33913d62bfe728dd500918472f2f8d4
mainframeai <martin@color-und-code.de>
2023-08-27 14:57:44 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,12 @@ export default function Home() {
           height={37}
           priority
         />
+         <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+             Testheadline
+            </span>
+          </h2>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
